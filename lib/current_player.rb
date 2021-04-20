ba00bf00569d9158934a0c8e703ff651157e12ee
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-04-20 19:20:59 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,7 +8,7 @@ end
 def turn_count(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   board.each do |turn|
     turns = 0
-    if |turn| = "X" || "O" turns +=1
+    if turn = "X" || "O" turns +=1
     end
     return turns
 end
