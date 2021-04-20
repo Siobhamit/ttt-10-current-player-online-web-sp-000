e6ed251ab916a5bd1efa12fca9cba9f5c8d5223c
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-04-20 19:25:19 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,15 +1,18 @@
+
+
+def turn_count(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  board.each do |turn|
+    turns = 0
+    if turn != ""
+       turns +=1
+    end
+    return turns
+end
+end
+
 def current_player(board)
   if turn_count(board).even? return "O"
 else "X"
 end
 
-end
-
-def turn_count(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
-  board.each do |turn|
-    turns = 0
-    if turn = "X" || "O" turns +=1
-    end
-    return turns
-end
 end
